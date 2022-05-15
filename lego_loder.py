301d777041fd95526e2ca24bad3984b0b6aa01b1
sleep2hours <1569171127@qq.com>
2022-05-15 17:34:08 +0800

    accomplished the dataloder of lego

---
+++ lego_loder.py
@@ -0,0 +1,67 @@
+from torch.utils import data
+import os
+import natsort
+import json
+import numpy as np
+import torch
+from PIL import Image
+from torchvision import transforms
+import  matplotlib.pyplot as plt
+
+class MyDataset(data.Dataset):   #return data (N*3*H*W)
+    def __init__(self,root_dir,transform=None,is_train=True):
+        super().__init__()
+        self.root_dir=root_dir
+        self.transform=transform
+        self.is_train=is_train
+        self.main_dir=self.root_dir+('train/' if is_train else 'test/')
+        img_names=list(filter(lambda x:x.endswith('png'),os.listdir(self.main_dir)))
+        self.imgs=natsort.natsorted(img_names)
+        self.cam_fov,self.tfs=self._camparamGet()
+
+    @staticmethod
+    def jsonRead(path:str):
+        with open(path,'r') as file:
+            items=json.load(file)
+        cam_fov=items["camera_angle_x"]
+        print('Camera fov: %lf'%(cam_fov))
+        tf_np=np.stack([frame["transform_matrix"] for frame in items["frames"]],axis=0)
+        tfs=torch.from_numpy(tf_np)
+        return cam_fov,tfs
+
+    def _camparamGet(self):
+        json_file="%stransforms_%s.json"%(self.root_dir,"train" if self.is_train else "test")
+        return MyDataset.jsonRead(json_file)
+
+    def cameraGet(self):
+        return self.cam_fov,self.tfs
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, index):
+        img_file=os.path.join(self.main_dir,self.imgs[index])
+        image=Image.open(img_file).convert('RGB')
+        img=self.transform(image)
+        return img,self.tfs[index]
+
+    def datasetGet(self):
+        result=[]
+        for im in self.imgs:
+            img_name=os.path.join(self.main_dir,im)
+            result.append(self.transform(Image.open(img_name).convert('RGB')))
+        all_images=torch.stack(result,dim=0)
+        return self.cam_fov,self.tfs,all_images
+
+if __name__=="__main__":
+    tf_func=transforms.Compose([transforms.Resize((400,400)),transforms.ToTensor(),])
+    dataset=MyDataset('./lego/',tf_func,is_train=True)
+    cam_fov,tfs,all_images=dataset.datasetGet()
+    print(tfs.size())
+    print(all_images.size())
+    for i in range(3):
+        plt.subplot(1,3,i+1)
+        plt.imshow(all_images[i*20].permute(1,2,0))
+    plt.show()
+
+
